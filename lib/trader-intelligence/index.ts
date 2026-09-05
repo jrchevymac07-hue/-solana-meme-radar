699ebf243cce2142a6a8f4c18b1cab7ea5dc3e14
jrchevymac07-hue <jrchevymac07@gmail.com>
2026-09-05 14:02:57 -0600

    Export trader intelligence primitives

---
+++ lib/trader-intelligence/index.ts
@@ -0,0 +1,11 @@
+export { aggregateTraderSignals } from "./aggregate";
+export { DisabledFomoProvider, boundTraderLimit, type TraderIntelligenceProvider } from "./provider";
+export { rankTraders, scoreTrader } from "./ranking";
+export type {
+  RankedTrader,
+  TokenTraderFeature,
+  TraderAction,
+  TraderPerformanceInput,
+  TraderSource,
+  TraderTokenSignal,
+} from "./types";
