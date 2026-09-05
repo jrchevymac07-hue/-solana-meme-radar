233ab2a75fd0c6436f2ca5f5052ca32aee8d1e14
jrchevymac07-hue <jrchevymac07@gmail.com>
2026-09-05 14:03:26 -0600

    Export Fomo discovery normalization

--- lib/trader-intelligence/index.ts
+++ lib/trader-intelligence/index.ts
@@ -1,4 +1,5 @@
 export { aggregateTraderSignals } from "./aggregate";
+export { normalizeFomoDiscovery, type FomoDiscoveredTrader } from "./fomo";
 export { DisabledFomoProvider, boundTraderLimit, type TraderIntelligenceProvider } from "./provider";
 export { rankTraders, scoreTrader } from "./ranking";
 export type {
